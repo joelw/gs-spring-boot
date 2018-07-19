fc9938089f28f6382e22df95f5a93afa465e7ddd
Joel Williams <joel@joelw.id.au>
2018-07-19 15:55:56 +0900

    Changes required for current Spring Boot version. Add Azure plugin.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -7,9 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -30,4 +32,8 @@ public class Application {
         };
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
